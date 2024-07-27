558b9f5b733436ddfac22cdb2047ebedd9b5168d
Aditya <117996501+Aditya41150@users.noreply.github.com>
2024-07-27 16:34:45 +0530

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,53 +1,90 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
-
-contract Token  {
-
+contract Token {
+    string public name = "Token";
+    string public symbol = "TKN";
+    uint8 public decimals = 18;
     uint256 private _totalSupply;
     address public owner;
     mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
 
-    constructor(uint256 initial_tokens){
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 initialTokens) {
         owner = msg.sender;
-        _balances[owner]+=initial_tokens;
-        _totalSupply+=initial_tokens;
+        _mint(owner, initialTokens);
     }
 
-    modifier onlyUser(){
-        require(msg.sender==owner,"sender is not the owner");
-        _;
-    }
-    modifier sufficientAmount(uint256 amount){
-        if(amount >_balances[msg.sender]){
-            revert("insufficient funds");
-        }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
         _;
     }
 
-    function mint(uint256 amount) public  onlyUser{
-        _totalSupply+=amount;
-        _balances[owner]+=amount;
+    modifier sufficientAmount(uint256 amount) {
+        require(amount <= _balances[msg.sender], "Insufficient funds");
+        _;
     }
 
-    function transfer(address to, uint256 amount) public sufficientAmount(amount) {
-        _balances[msg.sender]-=amount;
-        _balances[to]+=amount;
+    function mint(uint256 amount) public onlyOwner {
+        _mint(owner, amount);
+    }
+
+    function transfer(address to, uint256 amount) public sufficientAmount(amount) returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
     }
 
     function burn(uint256 amount) public sufficientAmount(amount) {
-        _balances[msg.sender]-=amount;
-        _totalSupply-=amount;
+        _burn(msg.sender, amount);
     }
 
-    function balance(address user) public view returns(uint256){
+    function balanceOf(address user) public view returns (uint256) {
         return _balances[user];
     }
 
-    function totalSupply() public view returns(uint256){
+    function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
-    
+    function approve(address spender, uint256 amount) public returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public sufficientAmount(amount) returns (bool) {
+        require(amount <= _allowances[from][msg.sender], "Allowance exceeded");
+        _transfer(from, to, amount);
+        _approve(from, msg.sender, _allowances[from][msg.sender] - amount);
+        return true;
+    }
+
+    function _mint(address account, uint256 amount) internal {
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        _totalSupply -= amount;
+        _balances[account] -= amount;
+        emit Transfer(account, address(0), amount);
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
 }
